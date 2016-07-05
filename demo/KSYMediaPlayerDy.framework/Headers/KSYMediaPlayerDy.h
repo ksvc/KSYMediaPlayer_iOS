09124d12273eac657d7331f0fa265ddb9f5171a6
shixuemei <shixuemei@kingsoft.com>
2016-07-05 20:07:19 +0800

    add Dynamic Link Library support

---
+++ demo/KSYMediaPlayerDy.framework/Headers/KSYMediaPlayerDy.h
@@ -0,0 +1,21 @@
+//
+//  KSYMediaDynamic.h
+//  KSYMediaDynamic
+//
+//  Created by zengfanping on 6/27/16.
+//  Copyright © 2016 bilibili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <KSYMediaPlayerDy/KSYMoviePlayerController.h>
+#import <KSYMediaPlayerDy/KSYMediaPlayback.h>
+
+//! Project version number for KSYMediaDynamic.
+FOUNDATION_EXPORT double KSYMediaDynamicVersionNumber;
+
+//! Project version string for KSYMediaDynamic.
+FOUNDATION_EXPORT const unsigned char KSYMediaDynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSYMediaDynamic/PublicHeader.h>
+
+
--- demo/KSYPlayerDemo/KSYPlayerVC.h
+++ demo/KSYPlayerDemo/KSYPlayerVC.h
@@ -5,9 +5,12 @@
 //  Created by yiqian on 11/3/15.
 //  Copyright (c) 2015 qyvideo. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
+#if USING_DYNAMIC_FRAMEWORK
+#import <KSYMediaPlayerDy/KSYMediaPlayerDy.h>
+#else
 #import <KSYMediaPlayer/KSYMediaPlayer.h>
+#endif
 @interface KSYPlayerVC : UIViewController
 - (instancetype)initWithURL:(NSURL *)url;
 @end
